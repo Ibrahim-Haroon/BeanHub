930bd013924c42d13e7d07b1f0b60aa9232ae20d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-25 01:45:01 -0500

    completed test cases for audio recorder.py

--- tests/test_audio_recorder.py
+++ tests/test_audio_recorder.py
@@ -1,14 +1,31 @@
 from scripts.audio_recorder import record_until_silence
+import speech_recognition as speech
+from mock import MagicMock, patch
+from pydub import AudioSegment
 
 script_path = 'scripts.audio_recorder'
 
 
-def test_record_until_silence_function_returns_none_when_no_audio():
-    # Arrange
+def test_record_until_silence_with_empty_audio_file():
+    audio_file_path = '/Users/ibrahimharoon/Python/BeanHub/tests/empty_audio.wav'
+    # audio_file = AudioSegment.from_file(audio_file_path, format="wav")
+
     expected_transcription = None
 
-    # Act
-    audio_data, transcribed_audio = record_until_silence()
+    _, actual_transcription = record_until_silence(audio_file_path)
 
-    # Assert
-    assert transcribed_audio == expected_transcription
+    assert expected_transcription == actual_transcription
+
+
+
+def test_record_until_silence_with_non_empty_audio_file():
+    # Arrange
+    audio_file_path = '/Users/ibrahimharoon/Python/BeanHub/tests/test_audio.wav'
+    # audio_file = AudioSegment.from_file(audio_file_path, format="wav")
+
+    expected_transcription = "this is a test"
+
+    _, actual_transcription = record_until_silence(audio_file_path)
+
+    assert expected_transcription == actual_transcription
+
