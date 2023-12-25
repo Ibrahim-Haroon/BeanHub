f7f937b1d63dfcbe39051c10abb2b51cadf8351d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-25 01:45:01 -0500

    completed test cases for audio recorder.py

--- scripts/audio_recorder.py
+++ scripts/audio_recorder.py
@@ -2,35 +2,52 @@ import speech_recognition as speech
 from pydub import AudioSegment
 
 
-def record_until_silence():
+def record_until_silence(source=None):
     recognizer = speech.Recognizer()
-    with speech.Microphone() as source:
-        print("Recording... Speak until you want to stop.")
+    audio_data = None
+    transcribed_audio = None
 
-        # Adjust for ambient noise
-        recognizer.adjust_for_ambient_noise(source)
-
+    if source is None:
         audio_data = []
-        transcribed_audio = None
+        with speech.Microphone as audio_source:
+            print("Recording... Speak until you want to stop.")
 
-        while True:
+            # Adjust for ambient noise
+            recognizer.adjust_for_ambient_noise(audio_source, duration=1)
+
+            while True:
+                try:
+                    audio_chunk = recognizer.listen(audio_source, timeout=2)
+                    audio_data.append(audio_chunk.frame_data)
+
+                    # Try to convert speech to text
+                    transcribed_audio = recognizer.recognize_google(audio_chunk)
+                    print(f"Recognized: {transcribed_audio}")
+
+                except speech.WaitTimeoutError:
+                    print("Timeout. No speech detected.")
+                    break
+                except speech.UnknownValueError:
+                    print("Could not understand audio.")
+                except speech.RequestError as e:
+                    print(f"Google Speech Recognition request failed: {e}")
+
+        audio_data = b"".join(audio_data)
+
+    else:
+        with speech.AudioFile(source) as audio_source:
+            audio = recognizer.record(audio_source)
+
             try:
-                audio_chunk = recognizer.listen(source, timeout=2)
-                audio_data.append(audio_chunk.frame_data)
-
-                # Try to convert speech to text
-                transcribed_audio = recognizer.recognize_google(audio_chunk)
-                print(f"Recognized: {transcribed_audio}")
-
-            except speech.WaitTimeoutError:
-                print("Timeout. No speech detected.")
-                break
+                transcribed_audio = recognizer.recognize_google(audio)
             except speech.UnknownValueError:
-                print("Could not understand audio.")
+                print("Google Speech Recognition could not understand audio")
+                return None, None
             except speech.RequestError as e:
-                print(f"Google Speech Recognition request failed: {e}")
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-        return b"".join(audio_data), transcribed_audio
+    return audio_data, transcribed_audio
+
 
 
 def save_as_mp3(audio_data, output_filename="recorded_audio.mp3"):
@@ -40,5 +57,6 @@ def save_as_mp3(audio_data, output_filename="recorded_audio.mp3"):
 
 
 if __name__ == "__main__":
-    recorded_audio, _ = record_until_silence()
-    save_as_mp3(recorded_audio)
+    recorded_audio, = record_until_silence('/Users/ibrahimharoon/Python/BeanHub/tests/test_audio.wav')
+    if recorded_audio is not None:
+        save_as_mp3(recorded_audio)
