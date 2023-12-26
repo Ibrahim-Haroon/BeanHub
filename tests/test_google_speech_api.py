d6b40b63f54801fcf74153c5e9566d6b6be3b89b
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-25 22:51:11 -0500

    updated google api test and updated file import in openai_text_api.py.

--- tests/test_google_speech_api.py
+++ tests/test_google_speech_api.py
@@ -1,17 +1,16 @@
 from scripts.ai_integration.google_speech_api import record_until_silence
 from os import path
 
-script_path = 'scripts.audio_recorder'
-
 
 def test_record_until_silence_with_empty_audio_file():
+    # Arrange
     audio_file_path = path.join(path.dirname(path.realpath(__file__)), "empty_audio.wav")
-    # audio_file = AudioSegment.from_file(audio_file_path, format="wav")
-
     expected_transcription = None
 
+    # Act
     _, actual_transcription = record_until_silence(audio_file_path)
 
+    # Assert
     assert expected_transcription == actual_transcription
 
 
@@ -19,11 +18,11 @@ def test_record_until_silence_with_empty_audio_file():
 def test_record_until_silence_with_non_empty_audio_file():
     # Arrange
     audio_file_path = path.join(path.dirname(path.realpath(__file__)), "test_audio.wav")
-    # audio_file = AudioSegment.from_file(audio_file_path, format="wav")
-
     expected_transcription = "this is a test"
 
+    # Act
     _, actual_transcription = record_until_silence(audio_file_path)
 
+    # Assert
     assert expected_transcription == actual_transcription
 
