4f8151869309eb54d9766148998aca00fddf3963
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-26 02:14:01 -0500

    having issue with connection, assuming it is with aws auth.

--- scripts/server/fill_vectordb.py
+++ scripts/server/fill_vectordb.py
@@ -1,7 +1,30 @@
 from langchain.vectorstores.pgvector import PGVector
+from scripts.ai_integration.openai_embeddings_api import *
+
+
+def connection_string() -> str:
+    connection_string_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other",
+                                            "connection_string")
+    with open(connection_string_file_path) as connection_str:
+        connection = connection_str.readline().strip()
+
+    return connection
+
+
+def fill_database(key: str = None):
+    menu = parse_menu_csv()
+    vectors, embeddings = embedding_api(menu, key)
+
+
+    db = PGVector.from_embeddings(text_embeddings=vectors, embedding=embeddings, metadatas=menu, collection_name="coffee_types", connection_string=connection_string())
 
 
 def main() -> int:
+    key_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "api_key")
+    with open(key_file_path) as api_key:
+        key = api_key.readline().strip()
+
+    fill_database(key)
 
     return 0
 
