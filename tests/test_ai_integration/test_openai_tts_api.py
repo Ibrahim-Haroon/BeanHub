930488ba87838a9ca9708bc434100fcb7b9e06a6
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-26 16:26:00 -0500

    implemented tests for openai_tts_api.

--- tests/test_ai_integration/test_openai_tts_api.py
+++ tests/test_ai_integration/test_openai_tts_api.py
@@ -11,22 +11,20 @@ def mock_openai(mocker):
     return mocker.patch(script_path + '.OpenAI')
 
 
-def test_openai_api(mock_openai):
+def test_openai_text_to_speech_api(mock_openai):
     # Arrange
-    mock_completion = MagicMock()
-    mock_completion.choices[0].message.content = "mocked_response"
-    mock_openai.return_value.chat.completions.create.return_value = mock_completion
+    mock_audio = MagicMock()
+    mock_audio.stream_to_file.return_value = None
+    mock_openai.return_value.audio.speech.create.return_value = mock_audio
 
     # Act
     with patch(script_path + '.OpenAI', return_value=mock_openai.return_value):
-        result = openai_text_to_speech_api(text="Test prompt", api_key="foo_key")
+        openai_text_to_speech_api(text="Test text", api_key="foo_key", audio_file_path="test_audio.mp3")
 
     # Assert
-    assert result == "mocked_response"
-    mock_openai.return_value.chat.completions.create.assert_called_once_with(
-        messages=[
-            {"role": "system", "content": "System message"},
-            {"role": "user", "content": "Test prompt"}
-        ],
-        model="gpt-4-1106-preview"
+    mock_openai.return_value.audio.speech.create.assert_called_once_with(
+        model="tts-1",
+        voice="alloy",
+        input="Test text"
     )
+    mock_audio.stream_to_file.assert_called_once_with("test_audio.mp3")
