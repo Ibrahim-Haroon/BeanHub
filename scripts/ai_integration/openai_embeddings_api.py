872dd6dcabba98029298a7f056b2f6794b9fb731
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-25 23:07:05 -0500

    openai_embeddings_api script works.

--- scripts/ai_integration/openai_embeddings_api.py
+++ scripts/ai_integration/openai_embeddings_api.py
@@ -3,12 +3,13 @@ from os import path
 
 
 def embedding_api(api_key: str = None):
+    embeddings = OpenAIEmbeddings(api_key=api_key)
 
-    return
+    print(embeddings.embed_query('black coffee'))
 
 
 def main() -> int:
-    key_file_path = path.join(path.dirname(path.realpath(__file__)), "api_key")
+    key_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "api_key")
     with open(key_file_path) as api_key:
         key = api_key.readline().strip()
 
