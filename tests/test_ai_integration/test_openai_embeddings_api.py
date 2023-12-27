ff7c947af94a07323383e950188f6807bd49e147
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-26 22:47:28 -0500

    updated tests for both functions.

--- tests/test_ai_integration/test_openai_embeddings_api.py
+++ tests/test_ai_integration/test_openai_embeddings_api.py
@@ -12,28 +12,26 @@ def mock_openai(mocker):
     return mocker.patch(script_path + '.OpenAIEmbeddings')
 
 
-def test_openai_embeddings_api(mock_openai_embeddings):
+def test_openai_embeddings_api(mock_openai):
     # Arrange
     expected_embeddings_instance = MagicMock()
-    mock_openai_embeddings.return_value = expected_embeddings_instance
-    expected_vectors = [1, 2]
-    expected_embeddings_instance.embed_query.side_effect = expected_vectors
+    mock_openai.return_value = expected_embeddings_instance
+    expected_vector = [1]
+    expected_embeddings_instance.embed_query.side_effect = expected_vector
 
     # Act
-    result_vectors, result_embeddings = openai_embedding_api(texts=["text1", "text2"], api_key="foo_key")
+    result_vector = openai_embedding_api(text="test", api_key="foo_key")
 
     # Assert
-    assert result_vectors == expected_vectors
-    assert result_embeddings == expected_embeddings_instance
-    mock_openai_embeddings.assert_called_once_with(api_key="foo_key")
+    assert result_vector == expected_vector[0]
+    mock_openai.assert_called_once_with(api_key="foo_key")
 
 
 
 def test_parse_menu_csv(mocker):
     # Arrange
-    expected_output = [
-        "{'MenuItem': {'itemName': 'test', 'price': 0.0}}",
-    ]
+    expected_output = [{'MenuItem': {'itemName': 'test', 'price': 0.0}}]
+
     mocker.patch('pandas.read_csv', return_value=pd.DataFrame({"item_name": ["test"], "price": [0.0]}))
 
     # Act
