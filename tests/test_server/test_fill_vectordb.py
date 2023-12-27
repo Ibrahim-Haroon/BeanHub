0ffc2e4424f5c2fa5e7d127e919652c68dbadedd
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-27 17:03:41 -0500

    more cleaned up version of the two succesful tests.

--- tests/test_server/test_fill_vectordb.py
+++ tests/test_server/test_fill_vectordb.py
@@ -7,35 +7,13 @@ from os import path
 
 @pytest.fixture
 def mock_components(mocker):
-    # Mocking the 'get_secret' function from 'script_path.aws_secret'
-    aws_secret_mock = mocker.patch('scripts.server.aws_secret.get_secret')
-
-    mock_api_key_data = 'your_mock_api_key_here'
-    mocker.patch('builtins.open', mock_open(read_data=mock_api_key_data), create=True)
-
-    # Mock the secret_file_path
-    secret_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "aws-info.csv")
-    api_key_file_path = key_file_path = path.join(path.dirname(path.realpath(__file__)), "../../other/" + "api_key")
-
-    # Mock the 'api_key' attribute or function in the 'other' module
-    api_key_mock = mocker.patch('other.api_key', return_value='mock_api_key')
-
-    # Mocking file operations and other components...
-    with patch('builtins.open', mock_open(read_data='...')):
-        mocker.patch('pandas.read_csv', return_value=pd.DataFrame({...}))
-        mocker.patch('os.path.exists', return_value=True)
-        mocker.patch('os.path.join', return_value=secret_file_path)
-        mocker.path('os.path.join', return_value=api_key_file_path)
-
-        return {
-            'api_key': api_key_mock,
-            'openai_embedding_api': mocker.patch('scripts.server.fill_vectordb.openai_embedding_api'),
-            'connection_string': mocker.patch('script_path.connection_string.connection_string'),
-            'register_vector': mocker.patch('pgvector.psycopg2.register_vector'),
-            'connect': mocker.patch('psycopg2.connect'),
-            'get_secret': aws_secret_mock,
-            'input': mocker.patch('builtins.input'),
-        }
+    return {
+        'openai_embedding_api': mocker.patch('scripts.server.fill_vectordb.openai_embedding_api'),
+        'connection_string': mocker.patch('script_path.connection_string.connection_string'),
+        'register_vector': mocker.patch('pgvector.psycopg2.register_vector'),
+        'connect': mocker.patch('psycopg2.connect'),
+        'input': mocker.patch('builtins.input'),
+    }
 
 
 @patch('builtins.input', return_value="NO")
