dee8319e900274239fc5abca18e651e9ce85c505
shafaatUML <shafaat.osmani@gmail.com>
2023-12-26 22:20:34 -0500

    Added navbar and button component

--- scripts/client/src/components/Navbar.js
+++ scripts/client/src/components/Navbar.js
@@ -1,44 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
-    const [click, setClick] = useState(false); 
-    const handleClick = () => setClick(!click); 
-    const closeMobileMenu => setClick(false); 
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
-    <>  
-        <nav className="navbar">
-            <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    BeanHub
-                </Link>
-                <div className='menu-icon'>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-Links' onClick={closeMobileMenu}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/services' className='nav-Links' onClick={closeMobileMenu}>
-                            Services
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                            Products
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                            Sign Up
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            BeanHub
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/about-us'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                About Us
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/features'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Features
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/try-beanhub'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Try BeanHub
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>TRY BEANHUB</Button>}
+        </div>
+      </nav>
     </>
   );
 }
