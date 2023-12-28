25cb42e66210b5294c43759bc4533adf97eeea61
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-27 23:18:47 -0500

    removed typo in file path variable.

--- scripts/server/connection_string.py
+++ scripts/server/connection_string.py
@@ -8,7 +8,7 @@ def connection_string() -> str:
     @rtype: str
     @return: connection string for PostgreSQL
     """
-    db_info_file_path = connection_string_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "database-info.csv")
+    db_info_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "database-info.csv")
 
     df = pd.read_csv(db_info_file_path)
     row = df.iloc[0]
