af8dc33913644a423fdba8c25eb8f03d21744e40
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-28 18:39:51 -0500

    emplemented similarity search function. Cannot do tests until nlp_bard gets implemented.

---
+++ scripts/server/similarity_search.py
@@ -0,0 +1,42 @@
+import psycopg2
+from io import StringIO
+from scripts.server.aws_secret import get_secret
+from scripts.ai_integration.openai_embeddings_api import *
+from scripts.server.connection_string import connection_string
+from scripts.ai_integration.nlp_bard import nlp_bard
+import numpy as np
+
+
+def similarity_search(order: str, key: str = None, aws_csv_file: StringIO = None, database_csv_file: StringIO = None) -> [[]]:
+    formatted_string = nlp_bard(order)
+
+    embedding = openai_embedding_api(formatted_string, key)
+
+
+    get_secret(aws_csv_file if not None else None)
+    db_connection = psycopg2.connect(connection_string(database_csv_file if not None else None))
+    db_connection.set_session(autocommit=True)
+
+    cur = db_connection.cursor()
+    cur.execute(""" SELECTED id, item_name, price, embeddings
+                    FROM products
+                    ORDER BY embeddings <-> %s limit 3;""",
+                (np.array(embedding),))
+    results = cur.fetchall()
+
+
+    return results
+
+
+def main() -> int:
+    key_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "api_key")
+    with open(key_path) as api_key:
+        key = api_key.readline().strip()
+
+    similarity_search(order="dummy", key=key)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
