826495f6282cf7ca666053d9a1ccb45c2eafd411
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-28 18:39:00 -0500

    stub until the dataset is completed and model can be trained.

---
+++ scripts/ai_integration/nlp_bard.py
@@ -0,0 +1,16 @@
+
+
+def load_str(string: str = None, file: str = None) -> str:
+    return "parse_data"
+
+
+def nlp_bard(input_string: str = None, file_path: str = None) -> str:
+    formatted_str = load_str()
+
+    # TODO: implement
+
+    return input_string
+
+
+if __name__ == "__main__":
+    nlp_bard()
