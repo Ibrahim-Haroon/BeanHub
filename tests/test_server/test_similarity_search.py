ed90262c403955fc732a4575cfc4f3f72e58de12
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-28 20:02:43 -0500

    completed first test, will now test case fails.

--- tests/test_server/test_similarity_search.py
+++ tests/test_server/test_similarity_search.py
@@ -1,20 +1,19 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from mock import patch, MagicMock
 from io import StringIO
 import csv
 from scripts.server.similarity_search import similarity_search
-from scripts.server.connection_string import connection_string
+
 
 @pytest.fixture
 def mock_components(mocker):
     return {
         'nlp_bard': mocker.patch('scripts.ai_integration.nlp_bard'),
-        'openai_embedding_api': mocker.patch('scripts.server.fill_vectordb.openai_embedding_api'),
+        'openai_embedding_api': mocker.patch('scripts.server.similarity_search.openai_embedding_api'),
         'connection_string': mocker.patch('scripts.server.connection_string.connection_string'),
-        'register_vector': mocker.patch('pgvector.psycopg2.register_vector'),
-        'connect': mocker.patch('scripts.server.fill_vectordb.psycopg2.connect'),
-        'input': mocker.patch('builtins.input'),
+        'connect': mocker.patch('scripts.server.similarity_search.psycopg2.connect'),
     }
+
 
 @pytest.fixture()
 def mock_boto3_session_client(mocker):
@@ -30,6 +29,20 @@ def as_csv_file(data: [[str]]) -> StringIO:
     return file_object
 
 
-def test_similarity_search(mocker):
+def test_similarity_search_returns_true_when_given_valid_params(mocker, mock_boto3_session_client, mock_components):
+    # Arrange
+    data = str({"input": {"Test"}})
+    key = "mock_api_key"
+    database_info = [
+        ["dbname", "user", "password", "host", "port"],
+        ["mydb", "myuser", "mypassword", "localhost", "port"]]
+    aws_info = [
+        ["secret_name", "region_name", "aws_access_key_id", "aws_secret_access_key"],
+        ["name", "us-east-1", "aws_access_key_id", "aws_secret_access_key"]]
 
-    assert 1 == 1
+    aws = as_csv_file(aws_info)
+    db = as_csv_file(database_info)
+
+    _, res = similarity_search(data, key=key, aws_csv_file=aws, database_csv_file=db)
+
+    assert res is True, f"expected search to be successful but {res}"
