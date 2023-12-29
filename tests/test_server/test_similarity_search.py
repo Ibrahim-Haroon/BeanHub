b335329286c5cf321ca9c308748a519a205254e0
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-28 19:33:17 -0500

    added fixture for boto3 aws SDK auth and as_csv_file to send authentication.

--- tests/test_server/test_similarity_search.py
+++ tests/test_server/test_similarity_search.py
@@ -15,33 +15,17 @@ def mock_components(mocker):
         'input': mocker.patch('builtins.input'),
     }
 
+@pytest.fixture()
+def mock_boto3_session_client(mocker):
+    return mocker.patch('boto3.session.Session.client', return_value=MagicMock())
 
-def test_similarity_search(mocker):
-    # Mocking necessary functions and objects
-    mock_cursor = MagicMock()
-    mock_connection = MagicMock()
-    mocker.mock_connect.return_value = mock_connection
-    mock_connection.cursor.return_value = mock_cursor
-    mocker.mock_nlp_bard.return_value = "formatted_string"
-    mocker.mock_openai_embedding_api.return_value = "fake_embedding"
 
-    # Input parameters
-    order = "dummy"
-    key = "fake_key"
-    aws_csv_file = StringIO("fake_aws_csv")
-    database_csv_file = StringIO("fake_db_csv")
+def as_csv_file(data: [[str]]) -> StringIO:
+    file_object = StringIO()
+    writer = csv.writer(file_object)
+    writer.writerows(data)
+    file_object.seek(0)
 
-    # Call the function
-    result = similarity_search(order, key, aws_csv_file, database_csv_file)
+    return file_object
 
-    # Assertions
-    mocker.mock_nlp_bard.assert_called_once_with(order)
-    mocker.mock_openai_embedding_api.assert_called_once_with("formatted_string", key)
-    mocker.mock_get_secret.assert_called_once_with(aws_csv_file)
-    mocker.mock_connect.assert_called_once_with(connection_string(database_csv_file))
-    mock_cursor.execute.assert_called_once_with(""" SELECTED id, item_name, price, embeddings
-                FROM products
-                ORDER BY embeddings <-> %s limit 3;""",
-            ("fake_embedding",))
-    mock_cursor.fetchall.assert_called_once()
-    assert result == mock_cursor.fetchall.return_value
+
