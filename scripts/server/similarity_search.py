828e775acab244ee642940a467244158827e45ff
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-28 19:02:11 -0500

    added new parameter K to determine the number of similar objects you want in return. Also added guard for false case.

--- scripts/server/similarity_search.py
+++ scripts/server/similarity_search.py
@@ -7,7 +7,20 @@ from scripts.ai_integration.nlp_bard import nlp_bard
 import numpy as np
 
 
-def similarity_search(order: str, key: str = None, aws_csv_file: StringIO = None, database_csv_file: StringIO = None) -> object:
+def similarity_search(order: str, top_k: int = 3, key: str = None, aws_csv_file: StringIO = None, database_csv_file: StringIO = None) -> object:
+    """
+
+    @rtype: list[list[float]] + boolean
+    @param order: customers order ex. "Can I have a black coffee with 3 shots of cream."
+    @param top_k: The number of closest embeddings you want
+    @param key: OpenAI auth
+    @param aws_csv_file: AWS SDK auth
+    @param database_csv_file: AWS RDS and PostgreSQL auth
+    @return: the list of 3 closest embeddings along with a boolean flag to mark success
+    """
+    if not order:
+        return None, False
+
     formatted_string = nlp_bard(order)
 
     embedding = openai_embedding_api(formatted_string, key)
@@ -18,9 +31,9 @@ def similarity_search(order: str, key: str = None, aws_csv_file: StringIO = None
     db_connection.set_session(autocommit=True)
 
     cur = db_connection.cursor()
-    cur.execute(""" SELECTED id, item_name, price, embeddings
+    cur.execute(f""" SELECTED id, item_name, price, embeddings
                     FROM products
-                    ORDER BY embeddings <-> %s limit 3;""",
+                    ORDER BY embeddings <-> %s limit {top_k};""",
                 (np.array(embedding),))
     results = cur.fetchall()
 
