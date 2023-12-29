121f3b352c053e9ab230fdf4b6280fcfbaccc56a
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-28 19:30:45 -0500

    moved all fixtures into one function.

--- tests/test_server/test_similarity_search.py
+++ tests/test_server/test_similarity_search.py
@@ -4,36 +4,26 @@ from io import StringIO
 from scripts.server.similarity_search import similarity_search
 from scripts.server.connection_string import connection_string
 
-
 @pytest.fixture
-def mock_nlp_bard():
-    with patch('scripts.ai_integration.nlp_bard') as mock:
-        yield mock
+def mock_components(mocker):
+    return {
+        'nlp_bard': mocker.patch('scripts.ai_integration.nlp_bard'),
+        'openai_embedding_api': mocker.patch('scripts.server.fill_vectordb.openai_embedding_api'),
+        'connection_string': mocker.patch('scripts.server.connection_string.connection_string'),
+        'register_vector': mocker.patch('pgvector.psycopg2.register_vector'),
+        'connect': mocker.patch('scripts.server.fill_vectordb.psycopg2.connect'),
+        'input': mocker.patch('builtins.input'),
+    }
 
-@pytest.fixture
-def mock_openai_embedding_api():
-    with patch('scripts.ai_integration.openai_embedding_api') as mock:
-        yield mock
 
-@pytest.fixture
-def mock_get_secret():
-    #Finish Writing where get_secret is
-    with patch('scripts.get_secret') as mock:
-        yield mock
-
-@pytest.fixture
-def mock_connect():
-    with patch('pgvector.psycopg2.connect') as mock:
-        yield mock
-
-def test_similarity_search(mock_nlp_bard, mock_openai_embedding_api, mock_get_secret, mock_connect):
+def test_similarity_search(mocker):
     # Mocking necessary functions and objects
     mock_cursor = MagicMock()
     mock_connection = MagicMock()
-    mock_connect.return_value = mock_connection
+    mocker.mock_connect.return_value = mock_connection
     mock_connection.cursor.return_value = mock_cursor
-    mock_nlp_bard.return_value = "formatted_string"
-    mock_openai_embedding_api.return_value = "fake_embedding"
+    mocker.mock_nlp_bard.return_value = "formatted_string"
+    mocker.mock_openai_embedding_api.return_value = "fake_embedding"
 
     # Input parameters
     order = "dummy"
@@ -45,10 +35,10 @@ def test_similarity_search(mock_nlp_bard, mock_openai_embedding_api, mock_get_se
     result = similarity_search(order, key, aws_csv_file, database_csv_file)
 
     # Assertions
-    mock_nlp_bard.assert_called_once_with(order)
-    mock_openai_embedding_api.assert_called_once_with("formatted_string", key)
-    mock_get_secret.assert_called_once_with(aws_csv_file)
-    mock_connect.assert_called_once_with(connection_string(database_csv_file))
+    mocker.mock_nlp_bard.assert_called_once_with(order)
+    mocker.mock_openai_embedding_api.assert_called_once_with("formatted_string", key)
+    mocker.mock_get_secret.assert_called_once_with(aws_csv_file)
+    mocker.mock_connect.assert_called_once_with(connection_string(database_csv_file))
     mock_cursor.execute.assert_called_once_with(""" SELECTED id, item_name, price, embeddings
                 FROM products
                 ORDER BY embeddings <-> %s limit 3;""",
