7049befc68148990c618077074770a46aaef5c2d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-12-30 16:06:16 -0500

    correct params passed to save model method.

--- scripts/ai_integration/bert_fine_tuning.py
+++ scripts/ai_integration/bert_fine_tuning.py
@@ -82,10 +82,10 @@ def fine_tune_bert(model_save_path: str = None) -> bool:
     model.train_model(train, eval_data=test, acc=accuracy_score)
 
     if model_save_path:
-        model.save_model(model_save_path)
+        model.save_model(model_save_path, model=model.model)
     else:
         save_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../..", "other/genai_models")
-        model.save_model(save_path)
+        model.save_model(save_path, model=model.model)
 
 
     return True
