e9736c6561bdecd5a02c340ce8223a4712c48056
shafaatUML <shafaat.osmani@gmail.com>
2023-12-31 22:56:47 -0500

    updated workflow again. all tests pass false positives

--- scripts/client/test_client/SpeechInput.test.js
+++ scripts/client/test_client/SpeechInput.test.js
@@ -14,3 +14,16 @@ describe('SpeechInput', () => {
 });
 
 */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Order from '../src/components/Order';
+
+describe('Order', () => {
+  it('renders without crashing', () => {
+    const { getByTestId } = render(<Order />);
+    const order = getByTestId('order');
+    expect(order).toBeInTheDocument();
+  });
+});
